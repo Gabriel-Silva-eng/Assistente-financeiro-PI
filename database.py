bee0d2a17055b1dfdc5d267481c8ddaee1283b3e
Gabriel_Silva <gabriel3012@gmail.com>
2026-04-09 21:34:49 -0300

    hotfix: Enviando as funções do banco de dados

--- database.py
+++ database.py
@@ -49,3 +49,39 @@ def excluir_transacao(id_transacao):
     
     conn.commit()
     conn.close()
+
+    
+def criar_tabela_lembretes():
+    conn = conectar()
+    cursor = conn.cursor()
+    cursor.execute('''
+        CREATE TABLE IF NOT EXISTS lembretes (
+            id INTEGER PRIMARY KEY AUTOINCREMENT,
+            titulo TEXT NOT NULL,
+            valor REAL NOT NULL,
+            data_vencimento DATE NOT NULL
+        )
+    ''')
+    conn.commit()
+    conn.close()
+
+def inserir_lembrete(titulo, valor, data):
+    conn = conectar()
+    cursor = conn.cursor()
+    cursor.execute('INSERT INTO lembretes (titulo, valor, data_vencimento) VALUES (?, ?, ?)', (titulo, valor, data))
+    conn.commit()
+    conn.close()
+
+def buscar_lembretes():
+    import pandas as pd
+    conn = conectar()
+    df = pd.read_sql_query("SELECT * FROM lembretes ORDER BY data_vencimento ASC", conn)
+    conn.close()
+    return df
+
+def excluir_lembrete(id_lembrete):
+    conn = conectar()
+    cursor = conn.cursor()
+    cursor.execute('DELETE FROM lembretes WHERE id = ?', (id_lembrete,))
+    conn.commit()
+    conn.close()
